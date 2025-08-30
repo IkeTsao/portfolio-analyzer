b0467458d0d774e602f140311ddca81a93130b79
IkeTsao <iketsao@gmail.com>
2025-08-30 17:34:21 +0800

    Update page.tsx
    fix compiler error

--- app/page.tsx
+++ app/page.tsx
@@ -140,7 +140,6 @@ export default function HomePage() {
             onAdd={handleAddHolding}
             onEdit={handleEditHolding}
             onRefresh={refreshHoldings}
-            onUpdatePrices={handleUpdatePrices}
           />
         </Stack>
       </Container>
